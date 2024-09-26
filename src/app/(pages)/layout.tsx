06db211b672c398d79be7f8706f6d71536a64281
Evgeniy Blank <blank.eugeny@yandex.ru>
2024-09-26 18:44:49 +0200

    init

---
+++ src/app/(pages)/layout.tsx
@@ -0,0 +1,22 @@
+import type { Metadata } from "next";
+import { Roboto } from "next/font/google";
+import "../globals.scss";
+
+const roboto = Roboto({ weight: "400", subsets: ["cyrillic"] });
+
+export const metadata: Metadata = {
+  title: "Tasty Creations",
+  description: "Сайт с рецептами блюд",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="ru">
+      <body className={`${roboto.className}`}>{children}</body>
+    </html>
+  );
+}
